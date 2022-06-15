a5197e8e87ea032a08b1b7bbfed4ba7f70b35bb2
Nicholas Marks <nickloop@gmail.com>
2022-06-15 19:00:41 +0000

    Challenge-Activity-21118c02-nickloop

---
+++ Challenge-Activity-21118c02-nickloop/main.py
@@ -0,0 +1,25 @@
+##
+## Welcome to 'Challenge Activity' 2.1.1.18.c02!
+## To complete the Challenge Activity, take a look 
+## at the image to the left (under main.py) and see if
+## you can reproduce the output. This time, however,
+## there are some constraints:
+##
+## 1. You have to use double quotes for all strings.
+## 2. You can not use print() function statements that
+##    have no arguments.
+##
+## Once you have completed this activity,
+## you can 'Submit' your file and view
+## the solution set for the activity or you can 'Fork'
+## the solution set to have a copy with which to work.
+## Good luck!
+##
+
+
+print("Rest APIs use Uniform Resource Identifiers (or URIs)")
+print("These are the parts of a URI:", end='\n\n')
+print("URI = scheme \"://\" authority \"/\" path [ \"?\" ] [ \"#\" fragment ]", end='\n\n')
+print("Think of the URI as the location of data")
+
+
