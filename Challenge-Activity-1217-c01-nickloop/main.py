87102e2553b1647d45306c57b7d1f43c9ea03ae2
Nicholas Marks <nickloop@gmail.com>
2022-06-15 18:31:23 +0000

    Challenge-Activity-1217-c01-nickloop

---
+++ Challenge-Activity-1217-c01-nickloop/main.py
@@ -0,0 +1,18 @@
+##
+## Welcome to your first 'Challenge Activity'!
+## This activity is based on Module #1: 1.2.1.7
+## To complete the Challenge Activity, take a look 
+## at the image to the left (under main.py) and see if
+## you can reproduce the output. Once you have completed
+## this activity, you can 'Submit' your file and view
+## the solution set for the activity or you can 'Fork'
+## the solution set to have a copy with which to work.
+## Good luck!
+##
+
+
+print('This line uses single quotes!',end='\n\n\n')
+
+print("This line uses double quotes...",end='\n\n')
+
+print("Lots of blank lines in between!")
