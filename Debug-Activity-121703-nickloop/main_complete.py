e6bad008ef7edf4c83f00a25cd7748c5101dc1cf
Nicholas Marks <nickloop@gmail.com>
2022-06-15 18:21:29 +0000

    Debug-Activity-121703

---
+++ Debug-Activity-121703-nickloop/main_complete.py
@@ -0,0 +1,9 @@
+##
+## This activity is based on Module 1.2.1.7 
+## There are three (3) errors in this Python file. See
+## if you can identify and then fix all errors so 
+## the program runs successfully. Once you have completed
+## this activity, you can 'Submit' your file and view
+## the solution set for the activity.
+##
+print("https://api.lunarcrush.com/v2?data=assets&key=abc123&symbol=JUNO")
