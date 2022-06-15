4900e8bda506668b40e1924975ea5d616977c60f
Nicholas Marks <nickloop@gmail.com>
2022-06-15 18:54:18 +0000

    Challenge-Activity-21118c01-nickloop

---
+++ Challenge-Activity-21118c01-nickloop/main.py
@@ -0,0 +1,21 @@
+##
+## Welcome to 'Challenge Activity' 2.1.1.18.c01!
+## To complete the Challenge Activity, take a look 
+## at the image to the left (under main.py) and see if
+## you can reproduce the output. This time, however,
+## there are some constraints:
+## 1. You have to use the keyword separators end/sep
+##    as part of your solution.
+## 2. You can only use a single print statement
+##
+## Once you have completed this activity,
+## you can 'Submit' your file and view
+## the solution set for the activity or you can 'Fork'
+## the solution set to have a copy with which to work.
+## Good luck!
+##
+
+
+print("The***","quick***", "sly***", "fox***", "jumped***", "over***", "the***", "lazy***", "dog***", sep='\n', end='\n')
+
+
